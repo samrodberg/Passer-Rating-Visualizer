368c418403d60259db27b8bfbd381fb3defd32c7
Sam R <shimp208@gmail.com>
2017-10-13 22:32:27 -0400

    Implemented methods for scraping data

--- DataParser.py
+++ DataParser.py
@@ -9,16 +9,48 @@ result = requests.get("http://www.nfl.com/stats/categorystats"
 
 c = result.text
 soup = BeautifulSoup(c, "html.parser")
-tables = soup.findChildren('table')
-my_table = tables[0]
 
-rows = my_table.findChildren(['th', 'tr'])
 
-# Get data from the table
-for row in rows:
-    cells = row.findChildren('td')
-    for cell in cells:
-        value = cell.string
-        if value is not None:
-            value = cell.string.split()
-            print value[-1]
+# Gets statistical data for QB's such as tds, yds, comp, att, etc. does not get name of player or team
+def get_qb_data(printing=True):
+    data = []
+    tables = soup.findChildren('table')
+    my_table = tables[0]
+    rows = my_table.findChildren(['th', 'tr'])
+    for row in rows:
+        cells = row.findChildren('td')
+        for cell in cells:
+            value = cell.string
+            if value is not None:
+                value = cell.string.split()
+                value = value[0].encode('utf-8')
+                data.append(value)
+                if printing is True:
+                    print value
+    if printing is True:
+        print data
+    return data
+
+# Gets QB's info specifically name and team
+def get_qb_info(printing=True):
+    players = soup.select("a[href^=/players/]")
+    teams = soup.select("a[href^=/teams/]")
+    players_list = [_.text.encode('utf-8') for _ in players]
+    teams_list = [_.text.encode('utf-8') for _ in teams]
+    if printing is True:
+        print players_list
+        print teams_list
+    qb_info = zip(players_list, teams_list)
+    return qb_info
+
+qb_data = get_qb_data(printing=False)
+qb_info = get_qb_info(printing=False)
+
+
+def format_data(printing=True):
+    qb_data = get_qb_data(printing=False)
+    qb_info = get_qb_info(printing=False)
+    print qb_data
+    print qb_info
+
+format_data(printing=True)
