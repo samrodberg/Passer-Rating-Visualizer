a0c87ea8d8c99915dc2cfdf59f348f9ceed9a9e7
Samuel Rodberg <shimp208@gmail.com>
2017-10-15 23:20:53 -0400

    Added bar chart for visualization
    Bar chart now shows data scrapped from NFL.com instead of dummy test data.

--- Visualizer.py
+++ Visualizer.py
@@ -1,16 +1,13 @@
-import PRFormulas as pr
+import DataParser
 import plotly
 from plotly.graph_objs import Bar, Layout
 
-brady = pr.calculate_pr_nfl(195.0, 133.0, 1702.0, 11.0, 1.0)
-palmer = pr.calculate_pr_nfl(227.0, 136.0, 1573.0, 6.0, 5.0)
-smith = pr.calculate_pr_nfl(158.0, 121.0, 1391.0, 11.0, 0.0)
-rodgers = pr.calculate_pr_nfl(189.0, 126.0, 1367.0, 13.0, 3.0)
+player_data = DataParser.format_data_list(printing=False)
 
-quarterback = ['Tom Brady', 'Carson Palmer', 'Alex Smith', 'Aaron Rodgers']
-ratings = [brady, palmer, smith, rodgers]
+ratings = [float(player_data[x][-1]) for x in range(0, len(player_data))]
+quarterbacks = [player_data[x][1] for x in range(0, len(player_data))]
 
 plotly.offline.plot({
-    "data": [Bar(x=quarterback, y=ratings)],
+    "data": [Bar(x=quarterbacks, y=ratings)],
     "layout": Layout(title="Passer Ratings")
 })
